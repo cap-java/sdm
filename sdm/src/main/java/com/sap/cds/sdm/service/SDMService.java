578e941db77cebd6673bce0602d637e296b60fe1
Rashmi <rashmi.angadi@sap.com>
2024-09-30 18:18:55 +0530

    SDMEXT-552:Delete Attachment/Entity

--- sdm/src/main/java/com/sap/cds/sdm/service/SDMService.java
+++ sdm/src/main/java/com/sap/cds/sdm/service/SDMService.java
@@ -1,25 +1,36 @@
 package com.sap.cds.sdm.service;
 
-import com.sap.cds.CdsData;
 import com.sap.cds.Result;
-import com.sap.cds.feature.attachments.generated.cds4j.sap.attachments.MediaData;
-import com.sap.cds.reflect.CdsEntity;
 import com.sap.cds.sdm.model.CmisDocument;
-import com.sap.cds.services.persistence.PersistenceService;
-import org.json.JSONObject;
 import com.sap.cds.sdm.model.SDMCredentials;
-
+import com.sap.cds.services.persistence.PersistenceService;
 import java.io.IOException;
-import java.util.List;
+import org.json.JSONObject;
 
 public interface SDMService {
-    public JSONObject createDocument(CmisDocument cmisDocument, String jwtToken, SDMCredentials sdmCredentials) throws IOException;
-    public String createFolder(String parentId, String jwtToken, String repositoryId, SDMCredentials sdmCredentials) throws IOException;
-    public String getFolderId(String jwtToken, Result result, PersistenceService persistenceService, String up__ID) throws IOException;
-    public String getFolderIdByPath(String parentId, String jwtToken, String repositoryId, SDMCredentials sdmCredentials) throws IOException;
-    public String checkRepositoryType(String repositoryId)throws IOException;
-    public JSONObject getRepositoryInfo(String token, SDMCredentials sdmCredentials) throws IOException ;
-    public Boolean isRepositoryVersioned(JSONObject repoInfo, String repositoryId) throws IOException;
-//    public void readDocument();
-//    public void deleteDocument();
+  public JSONObject createDocument(
+      CmisDocument cmisDocument, String jwtToken, SDMCredentials sdmCredentials) throws IOException;
+
+  public String createFolder(
+      String parentId, String jwtToken, String repositoryId, SDMCredentials sdmCredentials)
+      throws IOException;
+
+  public String getFolderId(
+      String jwtToken, Result result, PersistenceService persistenceService, String up__ID)
+      throws IOException;
+
+  public String getFolderIdByPath(
+      String parentId, String jwtToken, String repositoryId, SDMCredentials sdmCredentials)
+      throws IOException;
+
+  public String checkRepositoryType(String repositoryId) throws IOException;
+
+  public JSONObject getRepositoryInfo(String token, SDMCredentials sdmCredentials)
+      throws IOException;
+
+  public Boolean isRepositoryVersioned(JSONObject repoInfo, String repositoryId) throws IOException;
+
+  //    public void readDocument();
+  public int deleteDocument(String cmisaction, String objectId, String userEmail, String subdomain)
+      throws IOException;
 }
--- sdm/src/test/java/com/sap/cds/handler/SDMAttachmentsServiceHandlerTest.java
+++ sdm/src/test/java/com/sap/cds/handler/SDMAttachmentsServiceHandlerTest.java
@@ -1,24 +1,90 @@
-package com.sap.cds.handler;
+package com.sap.cds.sdm.service.handler;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
 
-import com.sap.cds.sdm.service.handler.SDMAttachmentsServiceHandler;
+import com.sap.cds.feature.attachments.service.model.servicehandler.AttachmentMarkAsDeletedEventContext;
+import com.sap.cds.feature.attachments.service.model.servicehandler.AttachmentReadEventContext;
+import com.sap.cds.reflect.CdsEntity;
+import com.sap.cds.reflect.CdsModel;
+import com.sap.cds.sdm.model.SDMCredentials;
+import com.sap.cds.sdm.persistence.DBQuery;
+import com.sap.cds.sdm.service.SDMService;
+import com.sap.cds.services.authentication.AuthenticationInfo;
+import com.sap.cds.services.authentication.JwtTokenAuthenticationInfo;
+import com.sap.cds.services.persistence.PersistenceService;
+import java.io.IOException;
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.MockitoAnnotations;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 public class SDMAttachmentsServiceHandlerTest {
-  private SDMAttachmentsServiceHandler handler;
+
+  @Mock private PersistenceService persistenceService;
+
+  @Mock private SDMService sdmService;
+
+  @Mock private AttachmentReadEventContext context;
+
+  @Mock private AttachmentMarkAsDeletedEventContext attachmentMarkAsDeletedEventContext;
+
+  @Mock private AuthenticationInfo authInfo;
+
+  @Mock private JwtTokenAuthenticationInfo jwtTokenInfo;
+
+  @Mock private SDMCredentials sdmCredentials;
+
+  @Mock private CdsModel cdsModel;
+
+  @Mock private CdsEntity cdsEntity;
+
+  @InjectMocks private SDMAttachmentsServiceHandler handler;
+
+  String objectId ="objectId";
+  String folderId="folderId";
+  String userEmail = "email";
+  String subdomain = "subdomain";
 
   @BeforeEach
-  void setUp() {
-    MockitoAnnotations.openMocks(this);
-    handler = new SDMAttachmentsServiceHandler();
+  public void setUp() {
+    when(attachmentMarkAsDeletedEventContext.getContentId())
+        .thenReturn("objectId:folderId:email:entity:subdomain");
   }
 
   @Test
-  void testPerformAction() {
-    String result = handler.performAction();
-    assertThat(result).isEqualTo("Action Performed");
+  public void testDocumentDeletion() throws IOException {
+    try (MockedStatic<DBQuery> mockedDBQuery = mockStatic(DBQuery.class)) {
+
+      when(attachmentMarkAsDeletedEventContext.getModel()).thenReturn(cdsModel);
+      when(cdsModel.findEntity(anyString())).thenReturn(Optional.of(cdsEntity));
+
+      mockedDBQuery
+          .when(() -> DBQuery.isFolderEmpty(cdsEntity, persistenceService, folderId))
+          .thenReturn(false);
+
+      handler.markAttachmentAsDeleted(attachmentMarkAsDeletedEventContext);
+      verify(sdmService).deleteDocument("delete", objectId, userEmail, subdomain);
+    }
+  }
+
+  @Test
+  public void testFolderDeletion() throws IOException {
+    try (MockedStatic<DBQuery> mockedDBQuery = mockStatic(DBQuery.class)) {
+
+      when(attachmentMarkAsDeletedEventContext.getModel()).thenReturn(cdsModel);
+      when(cdsModel.findEntity(anyString())).thenReturn(Optional.of(cdsEntity));
+      mockedDBQuery
+          .when(() -> DBQuery.isFolderEmpty(cdsEntity, persistenceService, folderId))
+          .thenReturn(true);
+      handler.markAttachmentAsDeleted(attachmentMarkAsDeletedEventContext);
+      verify(sdmService).deleteDocument("deleteTree", folderId, userEmail, subdomain);
+    }
   }
 }
