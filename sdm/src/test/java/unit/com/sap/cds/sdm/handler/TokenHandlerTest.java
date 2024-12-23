590f5c37205757a7c161cd80cdf704efec734634
Rishi Kunnath <82925475+rishikunnath2747@users.noreply.github.com>
2024-12-23 15:52:04 +0530

    fix workflows

--- sdm/src/test/java/unit/com/sap/cds/sdm/handler/TokenHandlerTest.java
+++ sdm/src/test/java/unit/com/sap/cds/sdm/handler/TokenHandlerTest.java
@@ -10,6 +10,7 @@ import static org.mockito.Mockito.*;
 import com.google.gson.JsonObject;
 import com.sap.cds.sdm.caching.CacheConfig;
 import com.sap.cds.sdm.caching.CacheKey;
+import com.sap.cds.sdm.handler.TokenHandler;
 import com.sap.cds.sdm.model.SDMCredentials;
 import com.sap.cloud.environment.servicebinding.api.DefaultServiceBindingAccessor;
 import com.sap.cloud.environment.servicebinding.api.ServiceBinding;
