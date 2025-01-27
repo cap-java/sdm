18145cc877b2c2f5d9b865c8b6efb5641041dcd4
Rashmi <rashmi.angadi@sap.com>
2025-01-27 13:10:43 +0530

    wedwed

--- sdm/src/main/java/com/sap/cds/sdm/service/handler/SDMAttachmentsServiceHandler.java
+++ sdm/src/main/java/com/sap/cds/sdm/service/handler/SDMAttachmentsServiceHandler.java
@@ -16,9 +16,83 @@ public class SDMAttachmentsServiceHandler implements EventHandler {
 
   @On(event = AttachmentService.EVENT_CREATE_ATTACHMENT)
   public void createAttachment(AttachmentCreateEventContext context) throws IOException {
-    var contentId = (String) context.getAttachmentIds().get(Attachments.ID);
-    context.setIsInternalStored(true);
-    context.setContentId(contentId);
+    String subdomain = "";
+    long startTime = System.currentTimeMillis() / 60000;
+    System.out.println("Start time " + startTime);
+    String repositoryId = SDMConstants.REPOSITORY_ID;
+    AuthenticationInfo authInfo = context.getAuthenticationInfo();
+    JwtTokenAuthenticationInfo jwtTokenInfo = authInfo.as(JwtTokenAuthenticationInfo.class);
+    String jwtToken = jwtTokenInfo.getToken();
+    String repocheck = sdmService.checkRepositoryType(jwtToken, repositoryId);
+    CmisDocument cmisDocument = new CmisDocument();
+    if ("Versioned".equals(repocheck)) {
+      throw new ServiceException(SDMConstants.VERSIONED_REPO_ERROR);
+    } else {
+      Map<String, Object> attachmentIds = context.getAttachmentIds();
+      String upID = (String) attachmentIds.get("up__ID");
+      CdsModel model = context.getModel();
+      Optional<CdsEntity> attachmentDraftEntity =
+          model.findEntity(context.getAttachmentEntity() + "_drafts");
+      Result result =
+          DBQuery.getAttachmentsForUPID(attachmentDraftEntity.get(), persistenceService, upID);
+      if (!result.list().isEmpty()) {
+        MediaData data = context.getData();
+
+        String filename = data.getFileName();
+        String fileid = (String) attachmentIds.get("ID");
+        String mimeType = (String) data.get("mimeType");
+        String errorMessageDI = "";
+        boolean nameConstraint = SDMUtils.isRestrictedCharactersInName(filename);
+        if (nameConstraint) {
+          throw new ServiceException(
+              SDMConstants.nameConstraintMessage(Collections.singletonList(filename), "Upload"));
+        }
+        Boolean duplicate = duplicateCheck(filename, fileid, result);
+        if (Boolean.TRUE.equals(duplicate)) {
+          throw new ServiceException(SDMConstants.getDuplicateFilesError(filename));
+        } else {
+          subdomain = TokenHandler.getSubdomainFromToken(jwtToken);
+          String folderId = sdmService.getFolderId(result, persistenceService, upID, jwtToken);
+          cmisDocument.setFileName(filename);
+          cmisDocument.setAttachmentId(fileid);
+          InputStream contentStream = (InputStream) data.get("content");
+          cmisDocument.setContent(contentStream);
+          cmisDocument.setParentId((String) attachmentIds.get("up__ID"));
+          cmisDocument.setRepositoryId(repositoryId);
+          cmisDocument.setFolderId(folderId);
+          cmisDocument.setMimeType(mimeType);
+          SDMCredentials sdmCredentials = TokenHandler.getSDMCredentials();
+          JSONObject createResult =
+              sdmService.createDocument(cmisDocument, sdmCredentials, jwtToken);
+
+          if (createResult.get("status") == "duplicate") {
+            throw new ServiceException(SDMConstants.getDuplicateFilesError(filename));
+          } else if (createResult.get("status") == "virus") {
+            throw new ServiceException(SDMConstants.getVirusFilesError(filename));
+          } else if (createResult.get("status") == "fail") {
+            errorMessageDI = createResult.get("message").toString();
+            throw new ServiceException(errorMessageDI);
+          } else {
+            cmisDocument.setObjectId(createResult.get("objectId").toString());
+            addAttachmentToDraft(attachmentDraftEntity.get(), persistenceService, cmisDocument);
+          }
+        }
+      }
+    }
+    context.setContentId(
+        cmisDocument.getObjectId()
+            + ":"
+            + cmisDocument.getFolderId()
+            + ":"
+            + context.getAttachmentEntity()
+            + ":"
+            + subdomain);
+    context.getData().setStatus("Clean");
+    context.getData().setContent(null);
+    long end = System.currentTimeMillis() / 60000;
+    long diff = end - startTime;
+    System.out.println("end time " + end);
+    System.out.println("Time taken  " + diff);
     context.setCompleted();
   }
 
