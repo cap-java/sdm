3ae72b2ea7a92e11060d14c9f5028dc020095bdc
Mahesh S <mahesh.sooryambylu@sap.com>
2025-04-21 23:50:48 +0530

    missed fix for retry

--- sdm/src/main/java/com/sap/cds/sdm/service/ReadAheadInputStream.java
+++ sdm/src/main/java/com/sap/cds/sdm/service/ReadAheadInputStream.java
@@ -110,6 +110,7 @@ public class ReadAheadInputStream extends InputStream {
                       }
                       return result;
                     })
+                .retryWhen(RetryUtils.retryLogic(5)) // Apply retry logic with 3 attempts
                 .toList()
                 .blockingGet();
 
