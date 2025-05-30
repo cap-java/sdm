65f34a1e227ae3bfe2daee07757294f62d4ec5a1
Akshat Jain <akshat.jain02@sap.com>
2025-05-30 17:27:21 +0530

    Added N-Attachment Test Case

--- sdm/src/test/java/integration/com/sap/cds/sdm/IntegrationTest_SingleFacet.java
+++ sdm/src/test/java/integration/com/sap/cds/sdm/IntegrationTest_SingleFacet.java
@@ -7,6 +7,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.time.LocalDateTime;
 import java.util.*;
 import okhttp3.*;
@@ -19,6 +21,7 @@ class IntegrationTest_SingleFacet {
   private static String entityID2;
   private static String facetName = "attachments";
   private static String entityID3;
+  private static String entityID4;
   private static String appUrl;
   private static String authUrl;
   private static String username;
@@ -32,6 +35,7 @@ class IntegrationTest_SingleFacet {
   private static String attachmentID2 = "";
   private static String attachmentID3 = "";
   private static String attachmentID4 = "";
+  private static String attachmentID5 = "";
 
   @BeforeAll
   static void setup() throws IOException {
@@ -1568,6 +1572,187 @@ class IntegrationTest_SingleFacet {
     }
     if (!testStatus) {
       fail("Could not update secondary property before entity is saved");
+    }
+  }
+
+  @Test
+  @Order(22)
+  void testNAttachments_NewEntity() throws IOException {
+    System.out.println(
+        "Test (23): Creating new entity and checking only max 4 attachments are allowed to be uploaded");
+    System.out.println("Creating entity");
+    Boolean testStatus = false;
+    String response = api.createEntityDraft(appUrl, serviceName, entityName, entityName2, srvpath);
+    if (response != "Could not create entity") {
+      entityID4 = response;
+
+      System.out.println("Entity created");
+
+      System.out.println("Creating attachment PDF");
+      ClassLoader classLoader = getClass().getClassLoader();
+
+      File file = new File(classLoader.getResource("sample.pdf").getFile());
+      Map<String, Object> postData1 = new HashMap<>();
+      postData1.put("up__ID", entityID4);
+      postData1.put("mimeType", "application/pdf");
+      postData1.put("createdAt", new Date().toString());
+      postData1.put("createdBy", "test@test.com");
+      postData1.put("modifiedBy", "test@test.com");
+
+      List<String> createResponse1 =
+          api.createFacet(
+              appUrl, serviceName, entityName, facetName, entityID4, srvpath, postData1, file);
+      if (createResponse1.get(0).equals("Attachment created")) {
+        attachmentID1 = createResponse1.get(1);
+        System.out.println("Attachment created");
+      }
+
+      System.out.println("Creating attachment TXT");
+      file = new File(classLoader.getResource("sample.txt").getFile());
+      Map<String, Object> postData2 = new HashMap<>();
+      postData2.put("up__ID", entityID4);
+      postData2.put("mimeType", "application/txt");
+      postData2.put("createdAt", new Date().toString());
+      postData2.put("createdBy", "test@test.com");
+      postData2.put("modifiedBy", "test@test.com");
+
+      List<String> createResponse2 =
+          api.createFacet(
+              appUrl, serviceName, entityName, facetName, entityID4, srvpath, postData2, file);
+      if (createResponse2.get(0).equals("Attachment created")) {
+        attachmentID2 = createResponse2.get(1);
+        System.out.println("Attachment created");
+      }
+
+      System.out.println("Creating attachment EXE");
+      file = new File(classLoader.getResource("sample.exe").getFile());
+      Map<String, Object> postData3 = new HashMap<>();
+      postData3.put("up__ID", entityID4);
+      postData3.put("mimeType", "application/exe");
+      postData3.put("createdAt", new Date().toString());
+      postData3.put("createdBy", "test@test.com");
+      postData3.put("modifiedBy", "test@test.com");
+
+      List<String> createResponse3 =
+          api.createFacet(
+              appUrl, serviceName, entityName, facetName, entityID4, srvpath, postData3, file);
+      if (createResponse3.get(0).equals("Attachment created")) {
+        attachmentID3 = createResponse3.get(1);
+        System.out.println("Attachment created");
+      }
+
+      System.out.println("Creating second attachment pdf");
+      file = new File(classLoader.getResource("sample1.pdf").getFile());
+      Map<String, Object> postData4 = new HashMap<>();
+      postData4.put("up__ID", entityID4);
+      postData4.put("mimeType", "application/pdf");
+      postData4.put("createdAt", new Date().toString());
+      postData4.put("createdBy", "test@test.com");
+      postData4.put("modifiedBy", "test@test.com");
+
+      List<String> createResponse4 =
+          api.createFacet(
+              appUrl, serviceName, entityName, facetName, entityID4, srvpath, postData3, file);
+      if (createResponse4.get(0).equals("Attachment created")) {
+        attachmentID4 = createResponse4.get(1);
+        System.out.println("Attachment created");
+      }
+
+      System.out.println("Creating third attachment pdf");
+      file = new File(classLoader.getResource("sample2.pdf").getFile());
+      Map<String, Object> postData5 = new HashMap<>();
+      postData5.put("up__ID", entityID4);
+      postData5.put("mimeType", "application/pdf");
+      postData5.put("createdAt", new Date().toString());
+      postData5.put("createdBy", "test@test.com");
+      postData5.put("modifiedBy", "test@test.com");
+
+      List<String> createResponse5 =
+          api.createFacet(
+              appUrl, serviceName, entityName, facetName, entityID4, srvpath, postData3, file);
+      if (createResponse5.get(0).equals("Only 4 attachments allowed.")) {
+        testStatus = true;
+        attachmentID5 = createResponse5.get(1);
+        System.out.println("Expected error received: Only 4 attachments allowed.");
+      }
+      String check = createResponse5.get(0);
+      if (check.equals("Attachment created")) {
+        testStatus = false;
+      } else {
+        response = api.saveEntityDraft(appUrl, serviceName, entityName, srvpath, entityID4);
+        if (response.equals("Saved")) {
+          String expectedJson =
+              "{\"error\":{\"code\":\"500\",\"message\":\"Only 4 attachments allowed.\"}}";
+          ObjectMapper objectMapper = new ObjectMapper();
+          JsonNode actualJsonNode = objectMapper.readTree(check);
+          JsonNode expectedJsonNode = objectMapper.readTree(expectedJson);
+          if (expectedJsonNode.equals(actualJsonNode)) {
+            testStatus = true;
+          }
+        }
+      }
+    }
+    if (!testStatus) {
+      fail("Attachment was created");
+    }
+  }
+
+  @Test
+  @Order(23)
+  void testUploadNAttachments() throws IOException {
+    System.out.println("Test (24): Upload maximum 4 attachments in an exsisting entity");
+
+    ClassLoader classLoader = getClass().getClassLoader();
+    File originalFile = new File(classLoader.getResource("sample.exe").getFile());
+
+    boolean testStatus = false;
+    String response = api.editEntityDraft(appUrl, serviceName, entityName, srvpath, entityID4);
+    System.out.println("response: " + response);
+
+    if ("Entity in draft mode".equals(response)) {
+      for (int i = 1; i <= 5; i++) {
+        // Ensure only one file is uploaded at a time and complete before next
+        File tempFile = File.createTempFile("sample_" + i + "_", ".exe");
+        Files.copy(originalFile.toPath(), tempFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+
+        Map<String, Object> postData = new HashMap<>();
+        postData.put("up__ID", entityID4);
+        postData.put("mimeType", "application/exe");
+        postData.put("createdAt", new Date().toString());
+        postData.put("createdBy", "test@test.com");
+        postData.put("modifiedBy", "test@test.com");
+
+        List<String> createResponse =
+            api.createFacet(
+                appUrl, serviceName, entityName, facetName, entityID4, srvpath, postData, tempFile);
+
+        String resultMessage = createResponse.get(0);
+        System.out.println("Result message for attachment " + i + ": " + resultMessage);
+
+        if (resultMessage.contains("Only 4 attachments allowed")) {
+          String expectedJson =
+              "{\"error\":{\"code\":\"500\",\"message\":\"Only 4 attachments allowed.\"}}";
+          ObjectMapper objectMapper = new ObjectMapper();
+          JsonNode actualJsonNode = objectMapper.readTree(resultMessage);
+          JsonNode expectedJsonNode = objectMapper.readTree(expectedJson);
+          if (expectedJsonNode.equals(actualJsonNode)) {
+            testStatus = true;
+          }
+        } else {
+          testStatus = false;
+        }
+        tempFile.delete();
+      }
+      if (!testStatus) {
+        fail("5th attachment did not trigger the expected error.");
+      }
+      // Delete the newly created entity
+      String deleteEntityResponse = api.deleteEntity(appUrl, serviceName, entityName, entityID4);
+      if (deleteEntityResponse != "Entity Deleted") {
+        fail("Could not delete entity");
+      } else {
+        System.out.println("Successfully deleted the test entity4");
+      }
     }
   }
 }
