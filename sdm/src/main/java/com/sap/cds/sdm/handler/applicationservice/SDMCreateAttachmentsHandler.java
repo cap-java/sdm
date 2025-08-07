3d014ffd9669cf872f20395f5551edaa0dcc28f8
Akshat Jain <akshat.jain02@sap.com>
2025-08-07 14:42:07 +0530

    [Added]: Whitespace Changes

--- sdm/src/main/java/com/sap/cds/sdm/handler/applicationservice/SDMCreateAttachmentsHandler.java
+++ sdm/src/main/java/com/sap/cds/sdm/handler/applicationservice/SDMCreateAttachmentsHandler.java
@@ -71,6 +71,7 @@ public class SDMCreateAttachmentsHandler implements EventHandler {
       List<String> filesNotFound = new ArrayList<>();
       List<String> filesWithUnsupportedProperties = new ArrayList<>();
       Map<String, String> badRequest = new HashMap<>();
+      List<String> fileWithWhiteSpace = new ArrayList<>();
       List<String> noSDMRoles = new ArrayList<>();
       for (Map<String, Object> entity : data) {
         List<Map<String, Object>> attachments = (List<Map<String, Object>>) entity.get(composition);
@@ -95,7 +96,8 @@ public class SDMCreateAttachmentsHandler implements EventHandler {
             composition,
             attachmentEntity,
             secondaryPropertiesWithInvalidDefinitions,
-            noSDMRoles);
+            noSDMRoles,
+            fileWithWhiteSpace);
         handleWarnings(
             context,
             fileNameWithRestrictedCharacters,
@@ -104,7 +106,8 @@ public class SDMCreateAttachmentsHandler implements EventHandler {
             filesWithUnsupportedProperties,
             badRequest,
             propertyTitles,
-            noSDMRoles);
+            noSDMRoles,
+            fileWithWhiteSpace);
       }
     }
   }
@@ -130,7 +133,8 @@ public class SDMCreateAttachmentsHandler implements EventHandler {
       String composition,
       Optional<CdsEntity> attachmentEntity,
       Map<String, String> secondaryPropertiesWithInvalidDefinitions,
-      List<String> noSDMRoles)
+      List<String> noSDMRoles,
+      List<String> fileWithWhiteSpace)
       throws IOException {
     List<Map<String, Object>> attachments = (List<Map<String, Object>>) entity.get(composition);
     if (attachments != null) {
@@ -146,7 +150,8 @@ public class SDMCreateAttachmentsHandler implements EventHandler {
             composition,
             attachmentEntity,
             secondaryPropertiesWithInvalidDefinitions,
-            noSDMRoles);
+            noSDMRoles,
+            fileWithWhiteSpace);
       }
       SecondaryPropertiesKey secondaryPropertiesKey =
           new SecondaryPropertiesKey(); // Emptying cache after attachments are updated in loop
@@ -166,7 +171,8 @@ public class SDMCreateAttachmentsHandler implements EventHandler {
       String composition,
       Optional<CdsEntity> attachmentEntity,
       Map<String, String> secondaryPropertiesWithInvalidDefinitions,
-      List<String> noSDMRoles)
+      List<String> noSDMRoles,
+      List<String> fileWithWhiteSpace)
       throws IOException {
     String id = (String) attachment.get("ID");
     String fileNameInDB;
@@ -230,8 +236,11 @@ public class SDMCreateAttachmentsHandler implements EventHandler {
           throw new ServiceException("Filename cannot be empty");
         }
       } else {
-        if (filenameInRequest == null) {
-          throw new ServiceException("Filename cannot be empty");
+        if (filenameInRequest == null || filenameInRequest.trim().length() == 0) {
+          fileWithWhiteSpace.add(fileNameInDB);
+          replacePropertiesInAttachment(
+              attachment, fileNameInDB, propertiesInDB, secondaryTypeProperties);
+
         } else if (!fileNameInDB.equals(
             filenameInRequest)) { // If the file name in DB is not equal to the file name in
           // request, it means that the file name has been modified
@@ -322,7 +331,8 @@ public class SDMCreateAttachmentsHandler implements EventHandler {
       List<String> filesWithUnsupportedProperties,
       Map<String, String> badRequest,
       Map<String, String> propertyTitles,
-      List<String> noSDMRoles) {
+      List<String> noSDMRoles,
+      List<String> fileWithWhiteSpace) {
     if (!fileNameWithRestrictedCharacters.isEmpty()) {
       context
           .getMessages()
@@ -362,6 +372,14 @@ public class SDMCreateAttachmentsHandler implements EventHandler {
     }
     if (!noSDMRoles.isEmpty()) {
       context.getMessages().warn(SDMConstants.noSDMRolesMessage(noSDMRoles, "create"));
+    }
+    if (!fileWithWhiteSpace.isEmpty()) {
+      context
+          .getMessages()
+          .warn(
+              String.format(
+                  SDMConstants.FILENAME_WHITESPACE_WARNING_MESSAGE,
+                  String.join(", ", fileWithWhiteSpace)));
     }
   }
 
