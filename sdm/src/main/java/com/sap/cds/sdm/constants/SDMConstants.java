0acfbb64f78632a339e9aaf7f9b4681f49c35aeb
Rashmi <rashmi.angadi@sap.com>
2025-09-23 15:58:24 +0530

    Update SDMConstants.java

--- sdm/src/main/java/com/sap/cds/sdm/constants/SDMConstants.java
+++ sdm/src/main/java/com/sap/cds/sdm/constants/SDMConstants.java
@@ -31,7 +31,7 @@ public class SDMConstants {
   public static final String VERSIONED_REPO_ERROR =
       "Upload not supported for versioned repositories.";
   public static final String VIRUS_REPO_ERROR_MORE_THAN_400MB =
-      "Upload not supported for file sizes greater than 400 when Repository is Virus Enabled.";
+      "Upload not supported for file sizes greater than 400MB when Repository is Virus Enabled.";
   public static final String VIRUS_ERROR = "%s contains potential malware and cannot be uploaded.";
   public static final String REPOSITORY_ERROR = "Failed to get repository info.";
   public static final String NOT_FOUND_ERROR = "Failed to read document.";
