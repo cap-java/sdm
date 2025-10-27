45d0bd9ef24dbfa7cc5869be2812b51640b8bb2a
yashmeet29 <160218676+yashmeet29@users.noreply.github.com>
2025-10-27 17:51:28 +0530

    Update SDMAttachmentsServiceHandler.java

--- sdm/src/main/java/com/sap/cds/sdm/service/handler/SDMAttachmentsServiceHandler.java
+++ sdm/src/main/java/com/sap/cds/sdm/service/handler/SDMAttachmentsServiceHandler.java
@@ -376,7 +376,7 @@ public class SDMAttachmentsServiceHandler implements EventHandler {
         throw new ServiceException(errorMessage2);
       default:
         cmisDocument.setObjectId(createResult.get("objectId").toString());
-        System.out.println("cmisDocument" + cmisDocument.getMimeType());
+        System.out.println("cmisDocument: \n" + cmisDocument.getMimeType());
         dbQuery.addAttachmentToDraft(
             getAttachmentDraftEntity(eventContext), persistenceService, cmisDocument);
         finalizeContext(eventContext, cmisDocument);
