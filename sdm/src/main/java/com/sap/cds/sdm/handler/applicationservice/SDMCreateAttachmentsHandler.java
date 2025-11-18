bdf7fbf5507b3b7b14a22a8f8c3cb82ee052e1e6
Rishi Kunnath <82925475+rishikunnath2747@users.noreply.github.com>
2025-11-18 09:56:39 +0530

    adding logs

--- sdm/src/main/java/com/sap/cds/sdm/handler/applicationservice/SDMCreateAttachmentsHandler.java
+++ sdm/src/main/java/com/sap/cds/sdm/handler/applicationservice/SDMCreateAttachmentsHandler.java
@@ -54,6 +54,9 @@ public class SDMCreateAttachmentsHandler implements EventHandler {
   @HandlerOrder(HandlerOrder.EARLY)
   public void processBefore(CdsCreateEventContext context, List<CdsData> data) throws IOException {
     // Get comprehensive attachment composition details for each entity
+    logger.info("CDS Data : " + data);
+    logger.info("Target Entity : " + context.getTarget().getQualifiedName());
+    logger.info("CDS Model : " + context.getModel());
     for (CdsData entityData : data) {
       Map<String, Map<String, String>> attachmentCompositionDetails =
           AttachmentsHandlerUtils.getAttachmentCompositionDetails(
--- sdm/src/main/java/com/sap/cds/sdm/handler/applicationservice/helper/AttachmentsHandlerUtils.java
+++ sdm/src/main/java/com/sap/cds/sdm/handler/applicationservice/helper/AttachmentsHandlerUtils.java
@@ -74,6 +74,8 @@ public class AttachmentsHandlerUtils {
           .compositions()
           .forEach(
               composition -> processDirectAttachmentComposition(entity, pathMapping, composition));
+      
+      logger.info("Path mapping after direct attachments: " + pathMapping.toString());
 
       // Process nested attachments
       entity
