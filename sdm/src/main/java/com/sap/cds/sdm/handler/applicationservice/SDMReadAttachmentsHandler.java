ff1bc174a5b5f7f852575faf850c6c98b14f0753
Rashmi <rashmi.angadi@sap.com>
2025-12-15 12:49:14 +0530

    Update SDMReadAttachmentsHandler.java

--- sdm/src/main/java/com/sap/cds/sdm/handler/applicationservice/SDMReadAttachmentsHandler.java
+++ sdm/src/main/java/com/sap/cds/sdm/handler/applicationservice/SDMReadAttachmentsHandler.java
@@ -67,7 +67,6 @@ public class SDMReadAttachmentsHandler implements EventHandler {
       //        dbQuery.updateNullUploadStatusToSuccess(
       //            attachmentDraftEntity.get(), persistenceService, upID, upIdKey);
       //      }
-      processAttachmentCriticality(context);
       if (repoValue.getIsAsyncVirusScanEnabled()) {
         processVirusScanInProgressAttachments(context);
       }
